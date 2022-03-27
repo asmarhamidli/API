48bf12ecb5742e52125383c3d1fc7d9e436b8cd1
Asmar <asmerhamidli@gmail.com>
2022-03-27 17:31:57 +0400

    first commit

--- Nasa API/main.js
+++ Nasa API/main.js
@@ -9,9 +9,12 @@ searchButton.addEventListener("click", ()=>{
 async function sendApiRequest (){
     let apiKey = 'WguyRyoA8sxCzbwy7b9NsQmUR3G5nb9g5bQtLJh3';
     let response = await fetch (`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
-    let data = response.json();
+    let data = response.json(); 
+    console.log(data);
+    useApiData(data);
 }
 
 function useApiData (data){
-
+document.getElementById("content").innerHTML += data.explanation;
+document.getElementById("content").innerHTML += `<img src="${data.url}">`
 }
