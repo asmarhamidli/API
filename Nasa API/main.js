ca86492322d9d1bdf7c66e0309a567d7e8ecf3fd
Asmar <asmerhamidli@gmail.com>
2022-03-27 18:09:45 +0400

    main

--- Nasa API/main.js
+++ Nasa API/main.js
@@ -2,20 +2,25 @@ let searchButton = document.getElementById("search");
 
 
 searchButton.addEventListener("click", ()=>{
+    console.log("button pressed")
     sendApiRequest()
 })
 
 
-async function sendApiRequest (){
-    let apiKey = 'WguyRyoA8sxCzbwy7b9NsQmUR3G5nb9g5bQtLJh3';
-    let response = await fetch (`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
+async function sendApiRequest() {
+    debugger
+   let API_KEY = "WguyRyoA8sxCzbwy7b9NsQmUR3G5nb9g5bQtLJh3";
+    debugger
+    let response = await fetch (`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
     console.log(response)
-    let data = response.json(); 
+    debugger
+    let data = await response.json(); 
+    debugger
     console.log(data);
     useApiData(data);
 }
 
 function useApiData (data){
-document.getElementById("content").innerHTML += data.status;
-document.getElementById("content").innerHTML += `<img src="${data.url}">`
+document.getElementById("content").innerHTML += data.explanation;
+document.getElementById("content").innerHTML += `<img class="img-fluid my-3" src="${data.url}">`
 }
