a6e4e6e9e29041ecd36babcf39953979089be5b0
Asmar <asmerhamidli@gmail.com>
2022-03-27 17:01:46 +0400

    commit on 'main'

---
+++ Nasa API/main.js
@@ -0,0 +1,15 @@
+let searchButton = document.getElementById("search");
+
+
+searchButton.addEventListener("click", ()=>{
+    sendApiRequest()
+})
+
+
+async function sendApiRequest (){
+    let response = await fetch ('');
+}
+
+function useApiData (data){
+    
+}
